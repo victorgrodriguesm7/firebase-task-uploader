7da9c47d82fb3f9e7e30c51f9a986665dc439678
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-04-24 14:48:39 -0300

    Read CSV, Convert to Task and Upload

---
+++ src/upload-csv/index.js
@@ -0,0 +1,77 @@
+const Fs = require('fs');
+var admin = require("firebase-admin");
+
+const CsvReadableStream = require('csv-reader');
+var serviceAccount = require("firebase-admin.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let inputStream = Fs.createReadStream('./out/data.csv', 'utf8');
+ 
+let data = [];
+
+function convertToObject(row){
+    let [
+        status,
+        title,
+        description,
+        onwer_id,
+        onwer_name,
+        history_1_id,
+        history_1_name,
+        history_1_action,
+        history_2_id,
+        history_2_name,
+        history_2_action,  
+    ] = row;
+
+    let Task = {
+        status,
+        title,
+        description,
+        onwer: {
+            onwer_id,
+            onwer_name,
+        },
+        history: [
+            {
+                id: history_1_id,
+                name: history_1_name,
+                action: history_1_action,             
+            },
+            {
+                id: history_2_id,
+                name: history_2_name,
+                action: history_2_action,
+            }
+        ]
+    };
+
+    data.push(Task);
+}
+
+function readCSV(){
+    inputStream
+        .pipe(new CsvReadableStream({ 
+            parseNumbers: true, 
+            parseBooleans: true, 
+            trim: true 
+        })).on('data', convertToObject).on('end', function (data) {
+            uplodToFirestore();
+        });
+}
+
+async function uplodToFirestore(){
+    let i = 1;
+    for (let Task of data){
+        await admin.firestore().collection('Tasks').add({
+            ...Task
+        });
+        console.log(`${i} Sent`)
+        i++;
+    }
+}
+
+readCSV();
